3094dad651857297ccd56fe0ecb95203e1a5a8e7
Krzysztof Pochwała <k.pochwala@gmail.com>
2021-03-26 00:38:48 +0100

    initial work on setting up setup.py for klippy

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'klipper',
+    version="0.9.0",
+    author="KevinOConnor",
+    description = 'Klipper 3D Printing Firmware',
+    packages=find_packages(),
+    entry_points = { "console_scripts": [ "klippy = klippy.klippy:main" ] },
+    url = "https://www.klipper3d.org/",
+    python_requires='>=3.5',
+)
